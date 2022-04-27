7b71b0a700d32e679aac68a812f99db4f5767452
suwan <blacksw1102@gmail.com>
2022-04-28 08:00:56 +0900

    mongodb document 생성,수정,삭제 추가

--- blob/main/app_mongodb.js
+++ blob/main/app_mongodb.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongodb = require("./mongoose");
 const Customer = require("./mongoose/schemas/customer");
-
 const app = express();
+
+// request body를 json으로 파싱 처리
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 
 mongodb.connect();
 
@@ -11,7 +17,9 @@ app.listen(3000, () => {
 });
 
 app.get("/customers", async (req, res) => {
-  console.log(await Customer.find());
+  console.log(req.body);
+  const result = await Customer.find();
+  res.send(result);
 
   // # Model find()
   // await MyModel.find({}); // 전체 문서 모두 조회
@@ -29,4 +37,24 @@ app.get("/customers", async (req, res) => {
   // await MyModel.findOne({ name : "John" }).exec(); // name이 John인 문서 한 건만 조회
   // MyModel.findOne({ name : 'John' }, function(err, doc) {}); // 쿼리 결과를 콜백 함수로 전달
   // await MyModel.findOne({ name : 'John' }, 'name email').exec(); // name이 John인 문서의 name 및 email 조회
+});
+
+app.post("/customer", async (req, res) => {
+  console.log(req.body);
+  const result = await Customer.create(req.body);
+  res.send(result);
+});
+
+app.put("/customer", async (req, res) => {
+  console.log(req.body);
+  const result = await Customer.updateMany({ name: /Customer/ }, { phone: "010-7777-5555" });
+  // await Customer.updateOne({ name: 'Jeremy Go' }, { phone: '010-7777-5555' });
+  res.send(result);
+});
+
+app.delete("/customer", async (req, res) => {
+  console.log(req.body);
+  const result = await Customer.deleteMany({ name: /John/ });
+  // await Customer.deleteOne({ name: 'Jeremy Go' });
+  res.send(result);
 });
