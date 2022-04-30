3be5d5cd7b3a12d4ed1a92dc4c90c11effcd5332
suwan <blacksw1102@gmail.com>
2022-04-30 19:50:01 +0900

    udp 전송 추가

---
+++ blob/main/udp/udp_client.js
@@ -0,0 +1,20 @@
+// Client
+const dgram = require("dgram");
+const toHost = "0.0.0.0";
+const toPort = 4000;
+const client = dgram.createSocket("udp4");
+
+const dataIdx = 1;
+let sum = "";
+
+for (let i = 0; i < 9216; i++) {
+  sum += dataIdx.toString();
+}
+
+const data = Buffer.from(sum);
+
+client.send(data, toPort, toHost, (err) => {
+  if (err) console.log(err);
+  else console.log("ok");
+  client.close();
+});
---
+++ blob/main/udp/udp_server.js
@@ -0,0 +1,16 @@
+// Server
+const dgram = require("dgram");
+const server = dgram.createSocket("udp4");
+const port = 4000;
+
+server.on("message", (message, info) => {
+  console.log(`message: ${message.toString()}`);
+  console.log(`from address: ${info.address} port: ${info.port}`);
+});
+
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`listening ${address.address}:${address.port}`);
+});
+
+server.bind(port);
